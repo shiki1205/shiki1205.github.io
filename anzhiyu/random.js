a258bcc8dc869ce5e2b4c92534297fb4437bf933
shiki1205 <1513632469@qq.com>
2024-10-30 16:49:32 +0800

    Site updated: 2024-10-30 16:49:32

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/4a17b156.html","posts/5bd2752a.html","posts/4a17b156.html"];function toRandomPost(){
+var posts=["posts/5bd2752a.html","posts/4a17b156.html","posts/4a17b156.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
