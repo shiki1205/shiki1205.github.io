0b41ea827cd68379b2fd639d27e4de3cf0b840f0
shiki1205 <1513632469@qq.com>
2024-12-03 00:55:30 +0800

    Site updated: 2024-12-03 00:55:30

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/31a7036b.html","posts/c4de2d78.html","posts/4a17b156.html","posts/5bd2752a.html","posts/66de4e4e.html","posts/4a17b156.html"];function toRandomPost(){
+var posts=["posts/31a7036b.html","posts/4a17b156.html","posts/5bd2752a.html","posts/4a17b156.html","posts/c4de2d78.html","posts/66de4e4e.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
