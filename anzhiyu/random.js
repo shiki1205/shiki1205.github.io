0b46f31ef8d4497d49be5b381ecf2158e2d67b18
shiki1205 <1513632469@qq.com>
2025-03-18 17:35:44 +0800

    Site updated: 2025-03-18 17:35:41

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/31a7036b.html","posts/0.html","posts/c4de2d78.html","posts/5bd2752a.html","posts/4a17b156.html","posts/66de4e4e.html"];function toRandomPost(){
+var posts=["posts/8a50a8e4.html","posts/5bd2752a.html","posts/31a7036b.html","posts/c4de2d78.html","posts/775c2c79.html","posts/4a17b156.html","posts/66de4e4e.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
