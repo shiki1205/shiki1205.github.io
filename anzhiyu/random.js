b2007f0f5a3b591a1bb65cd6fef02e625555bb7a
shiki1205 <1513632469@qq.com>
2025-04-19 18:17:53 +0800

    Site updated: 2025-04-19 18:17:53

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/c4de2d78.html","posts/31a7036b.html","posts/8a50a8e4.html","posts/5bd2752a.html","posts/66de4e4e.html","posts/775c2c79.html","posts/4a17b156.html"];function toRandomPost(){
+var posts=["posts/8a50a8e4.html","posts/31a7036b.html","posts/c4de2d78.html","posts/5bd2752a.html","posts/775c2c79.html","posts/4a17b156.html","posts/e9067f00.html","posts/a74db3c9.html","posts/700f2eba.html","posts/7081e2c.html","posts/66de4e4e.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
